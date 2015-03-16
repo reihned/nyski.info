f9ca015573268cba03584c195b6d20d44ca33bb6
Jonathan Eddy <jonathanceddy@gmail.com>
2015-03-16 09:23:18 -0400

    trying to do directions stuff

--- app/assets/javascripts/directions.js
+++ app/assets/javascripts/directions.js
@@ -1,8 +1,51 @@
 //can't figure out how to access api key in js file
 // keep getting this error: 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://localhost:3000' is therefore not allowed access.
+// function calculateRoute(from, to) {
+// 	$("#origin-form").submit(function(event) {
+// 	  event.preventDefault();
+// 	  var origin = $('#origin').val();
+// 		var destination = $(this).attr('data-id');
+// 	  calculateRoute(origin, destination);
+// 	});
+
+// 	var myOptions = {
+// 	  zoom: 12,
+// 	  center: new google.maps.LatLng(40.7127136, -73.978651),
+// 	  mapTypeId: google.maps.MapTypeId.ROADMAP
+// 	};
+
+// 	var directionsService = new google.maps.DirectionsService();
+
+// 	var directionsRequest = {
+// 	  origin: from,
+// 	  destination: to,
+// 	  travelMode: google.maps.DirectionsTravelMode.DRIVING,
+// 	  unitSystem: google.maps.UnitSystem.IMPERIAL
+// 	};
+
+// 	directionsService.route(
+// 	  directionsRequest,
+// 	  function(response, status)
+// 	  {
+// 	    if (status == google.maps.DirectionsStatus.OK)
+// 	    {
+// 	      new google.maps.DirectionsRenderer({
+// 	        map: mapObject,
+// 	        directions: response
+// 	      });
+// 	    }
+// 	    else
+// 	      $("#error").append("Unable to retrieve your route<br />");
+// 	  }
+// 	);
+// }
+
+// var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+
 
 $(function(){
 	console.log('loaded')
+
 
 	$('#origin-form').on('submit', function(e){
 		
@@ -14,7 +57,7 @@ $(function(){
 		$.ajax({
 			url: '/trips/' + destination,
 			method: 'GET',
-			dataType: 'json',
+			dataType: 'jsonp',
 			success: function(data){
 				findDirections(origin, data);
 			}
@@ -32,7 +75,7 @@ function findDirections(origin, destination){
 
 	$.ajax({
 		url: baseURL + origin + "&destination=" + destination + "&key=" + ApiKey,
-		dataType: 'json',
+		dataType: 'jsonp',
 		success:function(data){
 			console.log(data);
 
