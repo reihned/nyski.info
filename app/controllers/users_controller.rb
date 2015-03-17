9791908bde5c81e00f0c65b6ba3ee9656a40acd9
Vienne Chen <vienne.chen@gmail.com>
2015-03-17 12:06:16 -0400

    added some routing thing for new user trying to select a trip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,8 +53,12 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
-          format.html { redirect_to @user, notice: 'User was successfully created.' }
-          format.json { render :show, status: :created, location: @user }
+        if session[:search_id]
+          format.html { redirect_to "/search/#{session[:search_id]}" }
+        else
+          format.html { redirect_to user_path(@user) }
+        end
+        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
