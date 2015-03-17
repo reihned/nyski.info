c09ccefc5fa5bab90291726ce72c2af88a10e10e
Vienne Chen <vienne.chen@gmail.com>
2015-03-17 11:02:04 -0400

    cleaned up nav, links on user show page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -15,7 +15,11 @@ class TripsController < ApplicationController
 	end
 
 	def new
-		@trip = Trip.new
+		if !current_user
+			redirect_to new_user_path
+		else
+			@trip = Trip.new
+		end
 	end
 
 	def edit
@@ -25,19 +29,15 @@ class TripsController < ApplicationController
 	def create
 		@trip = Trip.new(trip_params)
 		@user = current_user
-			if current_user 
-				respond_to do |format|
-					if @trip.update(creator_id: @user.id) && @trip.update(status: 'pending')
-   					format.html { redirect_to @trip, notice: 'Trip done been made.' }
-        		format.json { render :show, status: :ok, location: @trip }
-      		else
-        		format.html { render :edit }
-        		format.json { render json: @trip.errors, status: :unprocessable_entity }
-      		end
-   			end
-    	else
-     		redirect_to new_user_path
-     	end
+		respond_to do |format|
+			if @trip.update(creator_id: @user.id) && @trip.update(status: 'pending')
+					format.html { redirect_to @trip, notice: 'Trip done been made.' }
+    		format.json { render :show, status: :ok, location: @trip }
+  		else
+    		format.html { render :edit }
+    		format.json { render json: @trip.errors, status: :unprocessable_entity }
+  		end
+		end
 	end
 
 	def update
