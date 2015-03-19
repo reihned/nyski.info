f0e05f8822cb5cf3bbd3a07128864d94ae06c286
reihned <reihned@gmail.com>
2015-03-19 14:10:22 -0400

    location id based reports

--- app/controllers/concerns/snow_report_api.rb
+++ app/controllers/concerns/snow_report_api.rb
@@ -15,5 +15,16 @@ module SnowReportApi
       }# options
     end # initialize
 
+    def locationReport(location_id)
+      options = @options
+      options[:query].merge!({"ids"=>location_id})
+
+      response = self.class.get("/conditions.php?",options)
+
+      return response["items"].first
+    end # location report
+
+    #temperature should be in fahrenheit
+
   end # class
 end # module
