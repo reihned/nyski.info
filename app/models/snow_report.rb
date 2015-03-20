f37d28b00f843cf0afd369f2078f66ed3f42abe2
reihned <reihned@gmail.com>
2015-03-20 14:10:42 -0400

    so, weird solution time

--- app/models/snow_report.rb
+++ app/models/snow_report.rb
@@ -1,14 +1,58 @@
-
-
 class SnowReport < ActiveRecord::Base
   include HTTParty
+  base_uri 'http://feeds.snocountry.net'
 
-  belongs_to :ski_location
+  #instanced model
   has_no_table
+  column :report_id,  :integer
+
+  attr_accessor :weather
 
   # base uri for httparty
   base_uri 'http://feeds.snocountry.net'
 
-  # So, assuming lazy loading...
+  after_initialize do
+    puts "after initialize"
+    @options = { query: {
+        "apiKey" => ENV["SNOCOUNTRY_API_KEY"],
+        "output" => "json"
+      }# query
+    }# options
+
+    fullReport = self.fullReport
+
+    self.weather = [
+      {
+        low: fullReport["weatherToday_Temperature_Low"],
+        high: fullReport["weatherToday_Temperature_High"]
+      },
+      {
+        low: fullReport["weatherTomorrow_Temperature_Low"],
+        high: fullReport["weatherTomorrow_Temperature_High"]
+      },
+      {
+        low: fullReport["weatherDayAfterTomorrow_Temperature_Low"],
+        high: fullReport["weatherDayAfterTomorrow_Temperature_High"]
+      },
+      {
+        low: fullReport["weatherDay4_Temperature_Low"],
+        high: fullReport["weatherDay4_Temperature_High"]
+      },
+      {
+        low: fullReport["weatherDay5_Temperature_Low"],
+        high: fullReport["weatherDay5_Temperature_High"]
+      },
+    ]
+  end
+
+  def fullReport(location_id=self.report_id)
+    options = @options
+    options[:query].merge!({"ids"=>location_id})
+
+    response = self.class.get("/conditions.php?",options)
+
+    return response["items"].first || "error"
+  end # location report
+
 
 end
